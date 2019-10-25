bf04de60516affc643806c799e38f770c3d9e503
burkhart <burkhart.joshua@gmail.com>
2019-10-25 04:36:18 -0700

    generalizing

--- build/create_dockerfile_gct2gctx.sh
+++ build/create_dockerfile_gct2gctx.sh
@@ -1,24 +1,39 @@
 #!/bin/bash
 # I don't see a need to ever chmod+x this file as it should be executed with sbatch.
 
+DOCKERFILE_PATH='../src/lib/aim1_genewise/dockerfiles/dockerfile_gct2gctx'
+
+#PYTHON3_PY_PATH='/Users/burkhajo/.conda/envs/reticula/lib/python3.7/site-packages/cmapPy/pandasGEXpress/gct2gctx.py'
+PYTHON3_PY_PATH='/home/burkhart/miniconda3/lib/python3.7/site-packages/cmapPy/pandasGEXpress
+
+
+DOCKER_ACCOUNT='burkhajo'
+
+DOCKER_CNT_IMG='gct2gctx'
+
+DOCKER_NAMESPC="$DOCKER_ACCOUNT/$DOCKER_CNT_IMG"
+
+EXACLD_ACCOUNT='burkhajo'
+
+LUSTRE_CNT_STR='/home/users/burkhajo/WuLab/WuLabLustreDir/reticula/bin/'
+
 #add dockerfile to context
-cp /Users/burkhajo/PycharmProjects/reticula/src/lib/aim1_genewise/dockerfiles/dockerfile_gct2gctx ./Dockerfile
+cp $DOCKERFILE_PATH ./Dockerfile
 
 #add executable to context
-cp /Users/burkhajo/.conda/envs/reticula/lib/python3.7/site-packages/cmapPy/pandasGEXpress/gct2gctx.py ./executable.py
+cp $PYTHON3_PY_PATH ./executable.py
 
 #docker login for tagging, save & push
 docker login
-docker build -t burkhajo/gct2gctx .
-docker save burkhajo/gct2gctx | gzip > ./savedGct2gctx.tar.gz
+docker build -t $DOCKER_NAMESPC .
+docker save $DOCKER_NAMESPC | gzip > ./saved$DOCKER_CNT_IMG.tar.gz
 
 #push to dockerhub
-docker push burkhajo/gct2gctx
+docker push $DOCKER_NAMESPC
 
 #tx to Exacloud Lustre fs
-scp ./savedGct2gctx.tar.gz burkhajo@exahead1.ohsu.edu:/home/users/burkhajo/WuLab/WuLabLustreDir/reticula/bin/
-
+scp ./saved$DOCKER_CNT_IMG.tar.gz $EXACLD_ACCOUNT@exahead1.ohsu.edu:$LUSTRE_CNT_STR
 #remove temporary files
 rm ./Dockerfile
 rm ./executable.py
-rm ./savedGct2gctx.tar.gz
+rm ./saved$DOCKER_CNT_IMG.tar.gz
