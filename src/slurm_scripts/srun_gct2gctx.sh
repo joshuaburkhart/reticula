ffa1da0fa547eb9ffeca5d39240ebde987ae1ec7
burkhart <burkhart.joshua@gmail.com>
2019-10-25 03:53:00 -0700

    optimizing scratch dir structure

--- src/slurm_scripts/srun_gct2gctx.sh
+++ src/slurm_scripts/srun_gct2gctx.sh
@@ -65,7 +65,7 @@ srun bzip2 -c /mnt/scratch/burkhajo/.data/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.
  > /mnt/scratch/burkhajo/.data/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.gctx.bz2
 
 # copy output to Lustre directory
-srun cp /mnt/scratch/burkhajo/.data/reticula/data/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.gctx.bz2 \
+srun cp /mnt/scratch/burkhajo/.data/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.gctx.bz2 \
  ~/WuLab/WuLabLustreDir/reticula/data/input/
 
 # clean node-local scratch directory
