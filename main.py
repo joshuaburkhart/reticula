b0edb410b6af6d7afa2af911762892689305c741
Joshua Burkhart <burkhart.joshua@gmail.com>
2019-11-20 16:40:51 -0800

    debugging configuration variable typos

--- main.py
+++ main.py
@@ -8,32 +8,36 @@ from dotenv import load_dotenv
 
 
 def execute_program() -> None:
-    os.chdir(os.getenv(varname='buildPath'))
-    os.system(f"sh {os.getenv(varname='buildInstFilNme')}")
+    os.chdir(os.getenv(key='buildPath'))
+    os.system(f"sh {os.getenv(key='buildInstFilNme')}")
 
 
 def generate_program_file_from_template(template_path: str,
                                         instance_name: str,
+                                        instance_extension: str,
                                         instance_permissions: int) -> None:
     with open(template_path, 'r') as build_template_filepointer:
         build_template = build_template_filepointer.read()
         build_script = Template(build_template).substitute(os.environ)
-    instance_path = f"{os.getenv(varname='buildPath')}/{instance_name}"
+    instance_path = f"{os.getenv(key='buildPath')}/{instance_name}{instance_extension}"
     with open(instance_path, 'w') as build_script_fp:
         build_script_fp.write(build_script)
         os.chmod(instance_path, instance_permissions)
 
 
 def generate_program_files() -> None:
-    generate_program_file_from_template(os.getenv(varname='buildTemplPath'),
-                                        os.getenv(varname='buildInstFilNme'),
-                                        0o555)  # set permissions to "r-xr-xr-x"
-    generate_program_file_from_template(os.getenv(varname='dockerTemplPath'),
-                                        os.getenv(varname='dockerInstFilNme'),
-                                        0o444)  # set permissions to "r--r--r--"
-    generate_program_file_from_template(os.getenv(varname='slurmTemplPath'),
-                                        os.getenv(varname='slurmInstFilNme'),
-                                        0o555)  # set permissions to "r-xr-xr-x"
+    generate_program_file_from_template(os.getenv(key='buildTemplPath'),
+                                        os.getenv(key='buildInstFilNme'),
+                                        os.getenv(key='buildInstExt'),
+                                        0o744)  # set permissions to "rwxr--r--"
+    generate_program_file_from_template(os.getenv(key='dockerTemplPath'),
+                                        os.getenv(key='dockerInstFilNme'),
+                                        os.getenv(key='dockerInstExt'),
+                                        0o644)  # set permissions to "rw-r--r--"
+    generate_program_file_from_template(os.getenv(key='slurmTemplPath'),
+                                        os.getenv(key='slurmInstFilNme'),
+                                        os.getenv(key='slurmInstExt'),
+                                        0o744)  # set permissions to "rwxr--r--"
 
 
 def build_parser() -> argparse.ArgumentParser:
@@ -43,7 +47,7 @@ def build_parser() -> argparse.ArgumentParser:
     return parser
 
 
-def main() -> None:
+if __name__ == '__main__':
     args = build_parser().parse_args(sys.argv[1:])
     env_path = Path(args.configuration)
     load_dotenv(dotenv_path=env_path)
