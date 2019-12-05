f61b2605786a7305827ba8f9c8bb75391a46f9a0
Joshua Burkhart <burkhart.joshua@gmail.com>
2019-12-04 18:07:19 -0800

    writing to new remote directory structure

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ def execute_program() -> None:
     os.system(f"sh {os.getenv(key='buildInstFilNme')}{os.getenv(key='buildInstExt')}")
 
 
-def generate_program_file_from_template(template_path: str,
-                                        instance_name: str,
-                                        instance_extension: str,
-                                        instance_permissions: int) -> None:
+def interpolate_template(template_path: str,
+                         instance_name: str,
+                         instance_extension: str,
+                         instance_permissions: int) -> None:
     with open(template_path, 'r') as build_template_filepointer:
         build_template = Template(build_template_filepointer.read())
         build_script = build_template.safe_substitute(os.environ)
@@ -25,19 +25,19 @@ def generate_program_file_from_template(template_path: str,
         os.chmod(instance_path, instance_permissions)
 
 
-def generate_program_files() -> None:
-    generate_program_file_from_template(os.getenv(key='buildTemplPath'),
-                                        os.getenv(key='buildInstFilNme'),
-                                        os.getenv(key='buildInstExt'),
-                                        0o744)  # set permissions to "rwxr--r--"
-    generate_program_file_from_template(os.getenv(key='dockerTemplPath'),
-                                        os.getenv(key='dockerInstFilNme'),
-                                        os.getenv(key='dockerInstExt'),
-                                        0o644)  # set permissions to "rw-r--r--"
-    generate_program_file_from_template(os.getenv(key='slurmTemplPath'),
-                                        os.getenv(key='slurmInstFilNme'),
-                                        os.getenv(key='slurmInstExt'),
-                                        0o744)  # set permissions to "rwxr--r--"
+def generate_executables() -> None:
+    interpolate_template(os.getenv(key='buildTemplPath'),
+                         os.getenv(key='buildInstFilNme'),
+                         os.getenv(key='buildInstExt'),
+                         0o744)  # set permissions to "rwxr--r--"
+    interpolate_template(os.getenv(key='dockerTemplPath'),
+                         os.getenv(key='dockerInstFilNme'),
+                         os.getenv(key='dockerInstExt'),
+                         0o644)  # set permissions to "rw-r--r--"
+    interpolate_template(os.getenv(key='slurmTemplPath'),
+                         os.getenv(key='slurmInstFilNme'),
+                         os.getenv(key='slurmInstExt'),
+                         0o744)  # set permissions to "rwxr--r--"
 
 
 def build_parser() -> argparse.ArgumentParser:
@@ -51,5 +51,5 @@ if __name__ == '__main__':
     args = build_parser().parse_args(sys.argv[1:])
     env_path = Path(args.configuration)
     load_dotenv(dotenv_path=env_path)
-    generate_program_files()
+    generate_executables()
     execute_program()
