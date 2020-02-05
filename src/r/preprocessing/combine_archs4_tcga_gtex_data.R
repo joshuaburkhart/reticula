6b60373ed08ead478c7f4d6cdc5b09a51ad60dfc
Joshua Burkhart <burkhart.joshua@gmail.com>
2020-02-05 13:36:51 -0800

    testing pheatmap generation

--- src/r/preprocessing/combine_archs4_tcga_gtex_data.R
+++ src/r/preprocessing/combine_archs4_tcga_gtex_data.R
@@ -1,6 +1,9 @@
 library(dplyr)
 library(biomaRt)
+library(viridis)
 library(magrittr)
+library(pheatmap)
+library(RColorBrewer)
 library(SummarizedExperiment)
 
 ensembl_dataset <- useEnsembl(biomart="ensembl",
@@ -170,3 +173,60 @@ tissue.vec <- c(archs4.tissue.vec,
                 gtex.tissue.vec)
 
 saveRDS(tissue.vec,file="~/tissue_vec.Rds")
+
+colFun <- colorRampPalette(RColorBrewer::brewer.pal(10,"Paired"))
+
+annotation_col.df <- data.frame(Datasource = datasource.vec,
+                                Tissue = tissue.vec)
+rownames(annotation_col.df) <- colnames(combined.df)
+
+Datasource <- colFun(length(unique(datasource.vec)))
+Tissue <- colFun(length(unique(tissue.vec)))
+
+names(Datasource) <- annotation_col.df$Datasource %>% unique()
+names(Tissue) <- annotation_col.df$Tissue %>% unique()
+
+anno_colors <- list(Datasource = Datasource,
+                    Tissue = Tissue)
+
+# combined.df columns ordered by datasource
+pheatmap::pheatmap(mat = combined.df,
+                   show_rownames = FALSE,
+                   show_colnames = FALSE,
+                   scale = "row",
+                   color = viridis::inferno(100),
+                   annotation_col = annotation_col.df,
+                   annotation_colors = anno_colors,
+                   cluster_cols = FALSE, # clustering takes a long time
+                   cluster_rows = FALSE, # clustering takes a long time
+                   silent = TRUE,
+                   filename = "~/combined_pheatmap_datasource.png",
+                   width=8,
+                   height=8,
+                   fontsize = 8
+)
+
+reorder.df <- data.frame(Sample=colnames(combined.df),
+                         Datasource=datasource.vec,
+                         Tissue=tissue.vec)
+ordered.tissues.df <- reorder.df %>%
+  dplyr::arrange(Tissue)
+
+combined.df <- combined.df[,ordered.tissues.df$Sample]
+
+# combined.df columns ordered by tissue
+pheatmap::pheatmap(mat = combined.df,
+                   show_rownames = FALSE,
+                   show_colnames = FALSE,
+                   scale = "row",
+                   color = viridis::inferno(100),
+                   annotation_col = annotation_col.df,
+                   annotation_colors = anno_colors,
+                   cluster_cols = FALSE, # clustering takes a long time
+                   cluster_rows = FALSE, # clustering takes a long time
+                   silent = TRUE,
+                   filename = "~/combined_pheatmap_tissue.png",
+                   width=8,
+                   height=8,
+                   fontsize = 8
+)
