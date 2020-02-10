4ef157c38629261dc578a2ab3c18ee49abf6fd24
burkhart <burkhart.joshua@gmail.com>
2020-02-09 17:52:59 -0800

    generating / tuning phate mds figures

--- src/r/preprocessing/snm.R
+++ src/r/preprocessing/snm.R
@@ -40,17 +40,68 @@ vst.rbe.t <- t(vst.rbe)
 
 # rm(vst.rbe)
 
+# vst + rbe
 phate.data <- phateR::phate(vst.rbe.t,
                             n.jobs=-2)
 
 saveRDS(phate.data,
         "~/phate.data")
 
+# sqrt + snm
+snmR.sqrt.cad <- readRDS("~/snmR_sqrt_cad.Rds")
+colnames(snmR.sqrt.cad$norm.dat) <- colnames(snmR.sqrt.cad$raw.dat)
+
+phate.snm.data <- phateR::phate(t(snmR.sqrt.cad$norm.dat),
+                                n.jobs=-2)
+
+saveRDS(phate.snm.data,
+        "~/phate.snm.data.Rds")
+
+# sqrt
+saveRDS(sqrt.combined.mtx,
+        "~/sqrt_combined_mtx.Rds")
+
+phate.sqrt.data <- phateR::phate(t(sqrt.combined.mtx),
+                                 n.jobs = -2)
+
+saveRDS(phate.sqrt.data,
+        "~/phate_sqrt_data.Rds")
+
 palette(rainbow(10))
-plot(phate.data,
-     col=as.numeric(as.factor(tissue.vec)))
-plot(phate.data,
-     col=as.numeric(as.factor(datasource.vec)))
+plot(phate.sqrt.data,
+     col=as.numeric(as.factor(tissue.vec))^2)
+plot(phate.sqrt.data,
+     col=as.numeric(as.factor(datasource.vec))^2)
+
+# vst 
+vst.mtx.counts <- as.matrix(assay(vst.counts))
+
+phate.vst.data <- phateR::phate(t(vst.mtx.counts),
+                                 n.jobs = -2)
+
+saveRDS(phate.vst.data,
+        "~/phate_vst_data.Rds")
+
+palette(rainbow(10))
+plot(phate.vst.data,
+     col=as.numeric(as.factor(tissue.vec))^2)
+plot(phate.vst.data,
+     col=as.numeric(as.factor(datasource.vec))^2)
+
+# combined.df
+com.mtx.counts <- as.matrix(combined.df)
+
+phate.com.data <- phateR::phate(t(com.mtx.counts),
+                                 n.jobs = -2)
+
+saveRDS(phate.com.data,
+        "~/phate_com_data.Rds")
+
+palette(rainbow(10))
+plot(phate.com.data,
+     col=as.numeric(as.factor(tissue.vec))^2)
+plot(phate.com.data,
+     col=as.numeric(as.factor(datasource.vec))^2)
 
 snmR.cad <- snm(vst.mtx.counts,
                bio.var=data.frame(tissue=tissue.vec),
