3b77d5806ab3965a2baa32e6b448df7d4fa86675
Joshua Burkhart <burkhart.joshua@gmail.com>
2020-02-26 17:57:17 -0800

    finished edits for progress report

--- src/r/preprocessing/combine_archs4_tcga_gtex_data.R
+++ src/r/preprocessing/combine_archs4_tcga_gtex_data.R
@@ -7,8 +7,8 @@ ensembl_dataset <- useEnsembl(biomart="ensembl",
                               dataset = "hsapiens_gene_ensembl",
                               mirror="uswest")
 
-ARCHS4_DATA_DIR <- "/home/users/burkhajo/WuLab/WuLabLustreDir/reticula/input/ARCHS4/"
-#ARCHS4_DATA_DIR <- "/Users/burkhajo/Software/reticula/data/aim1/input/ARCHS4/"
+#ARCHS4_DATA_DIR <- "/home/users/burkhajo/WuLab/WuLabLustreDir/reticula/input/ARCHS4/"
+ARCHS4_DATA_DIR <- "/Users/burkhajo/Software/reticula/data/aim1/input/ARCHS4/"
 ARCHS4_ESGS_FIL <- "Esophagus_expression_matrix.tsv"
 ARCHS4_COLN_FIL <- "Colon_expression_matrix.tsv"
 ARCHS4_LIVR_FIL <- "Liver_expression_matrix.tsv"
@@ -30,14 +30,18 @@ archs4.df <- archs4.esgs.df %>%
 rownames(archs4.df) <- rownames(archs4.kdny.df) # all archs4 dataframe rows match
 
 archs4.df <- archs4.df[rowSums(archs4.df) > 0, colSums(archs4.df) > 0] %>% na.omit(.)
-                
+
+archs4.zero.na.dropped.transcripts <- setdiff(rownames(archs4.kdny.df),rownames(archs4.df))
+
 archs4.ensembl_gene_map <- getBM(attributes = c("ensembl_gene_id_version","external_gene_name"),
                                  filters="external_gene_name",
                                  values=row.names(archs4.df),
                                  mart=ensembl_dataset)
 
-TCGA_DATA_DIR <- "/home/users/burkhajo/WuLab/WuLabLustreDir/reticula/input/recount2/recount2edTCGA/"
-#TCGA_DATA_DIR <- "/Users/burkhajo/Software/reticula/data/aim1/input/recount2/recount2edTCGA/"
+archs4.map.dropped.transcripts <- setdiff(rownames(archs4.df),archs4.ensembl_gene_map$external_gene_name)
+
+#TCGA_DATA_DIR <- "/home/users/burkhajo/WuLab/WuLabLustreDir/reticula/input/recount2/recount2edTCGA/"
+TCGA_DATA_DIR <- "/Users/burkhajo/Software/reticula/data/aim1/input/recount2/recount2edTCGA/"
 TCGA_ESGS_FIL <- "rse_gene_esophagus.Rdata"
 TCGA_COLN_FIL <- "rse_gene_colorectal.Rdata"
 TCGA_LIVR_FIL <- "rse_gene_liver.Rdata"
@@ -70,13 +74,17 @@ rownames(tcga.df) <- rownames(tcga.kdny.df) # all tcga dataframe rows match
 
 tcga.df <- tcga.df[rowSums(tcga.df) > 0,colSums(tcga.df) > 0] %>% na.omit(.)
 
+tcga.zero.na.dropped.transcripts <- setdiff(rownames(tcga.kdny.df),rownames(tcga.df))
+
 tcga.ensembl_gene_map <- getBM(attributes = c("ensembl_gene_id_version","external_gene_name"),
                                filters="ensembl_gene_id_version",
                                values=row.names(tcga.df),
                                mart=ensembl_dataset)
 
-GTEx_DATA_DIR <- "/home/users/burkhajo/WuLab/WuLabLustreDir/reticula/input/recount2/recount2edGTEx/"
-#GTEx_DATA_DIR <- "/Users/burkhajo/Software/reticula/data/aim1/input/recount2/recount2edGTEx/"
+tcga.map.dropped.transcripts <- setdiff(rownames(tcga.df),tcga.ensembl_gene_map$ensembl_gene_id_version)
+
+#GTEx_DATA_DIR <- "/home/users/burkhajo/WuLab/WuLabLustreDir/reticula/input/recount2/recount2edGTEx/"
+GTEx_DATA_DIR <- "/Users/burkhajo/Software/reticula/data/aim1/input/recount2/recount2edGTEx/"
 GTEx_ESGS_FIL <- "rse_gene_esophagus.Rdata"
 GTEx_COLN_FIL <- "rse_gene_colon.Rdata"
 GTEx_LIVR_FIL <- "rse_gene_liver.Rdata"
@@ -104,16 +112,68 @@ rownames(gtex.df) <- rownames(gtex.kdny.df) # all gtex dataframe rows match
 
 gtex.df <- gtex.df[rowSums(gtex.df) > 0, colSums(gtex.df) > 0] %>% na.omit(.)
 
+gtex.zero.na.dropped.transcripts <- setdiff(rownames(gtex.kdny.df),rownames(gtex.df))
+
 gtex.ensembl_gene_map <- getBM(attributes = c("ensembl_gene_id_version","external_gene_name"),
                                filters="ensembl_gene_id_version",
                                values=row.names(gtex.df),
                                mart=ensembl_dataset)
 
+gtex.map.dropped.transcripts <- setdiff(rownames(gtex.df),gtex.ensembl_gene_map$ensembl_gene_id_version)
+
+union.ensembl.gene.ids <- union(archs4.ensembl_gene_map$ensembl_gene_id_version,
+                                union(tcga.ensembl_gene_map$ensembl_gene_id_version,
+                                      gtex.ensembl_gene_map$ensembl_gene_id_version))
+
 shared.ensembl.gene.ids <- intersect(archs4.ensembl_gene_map$ensembl_gene_id_version,
                                      intersect(tcga.ensembl_gene_map$ensembl_gene_id_version,
                                                gtex.ensembl_gene_map$ensembl_gene_id_version))
 
-#12560 rows shared among ensembl_gene_id_version columns of archs4 & tcga/gtex
+ensembl.mapped.dropped.transcript.df <- data.frame(ARCHS4=as.numeric(union.ensembl.gene.ids %in% archs4.ensembl_gene_map$ensembl_gene_id_version),
+                                                      TCGA=as.numeric(union.ensembl.gene.ids %in% tcga.ensembl_gene_map$ensembl_gene_id_version),
+                                                      GTEX=as.numeric(union.ensembl.gene.ids %in% gtex.ensembl_gene_map$ensembl_gene_id_version),
+                                                   row.names = union.ensembl.gene.ids)
+
+ensembl.mapped.dropped.transcript.df <- ensembl.mapped.dropped.transcript.df[rowSums(ensembl.mapped.dropped.transcript.df) < 3,]
+
+write.csv(ensembl.mapped.dropped.transcript.df,
+          file="~/dropped_transcripts_df.csv")
+
+write.table(rownames(ensembl.mapped.dropped.transcript.df) %>% gsub("\\..+","",.),
+            file="~/dropped_transcripts.csv",
+            row.names=FALSE,
+            col.names = FALSE,
+            quote=FALSE)
+write.table(archs4.zero.na.dropped.transcripts %>% gsub("\\..+","",.),
+            file="~/archs4_zero_na_dropped_transcripts.csv",
+            row.names = FALSE,
+            col.names = FALSE,
+            quote=FALSE)
+write.table(archs4.map.dropped.transcripts %>% gsub("\\..+","",.),
+            file="~/archs4_map_dropped_transcripts.csv",
+            row.names=FALSE,
+            col.names=FALSE,
+            quote=FALSE)
+write.table(tcga.zero.na.dropped.transcripts %>% gsub("\\..+","",.),
+            file="~/tcga_zero_na_dropped_transcripts.csv",
+            row.names = FALSE,
+            col.names = FALSE,
+            quote=FALSE)
+write.table(tcga.map.dropped.transcripts %>% gsub("\\..+","",.),
+            file="~/tcga_map_dropped_transcripts.csv",
+            row.names=FALSE,
+            col.names=FALSE,
+            quote=FALSE)
+write.table(gtex.zero.na.dropped.transcripts %>% gsub("\\..+","",.),
+            file="~/gtex_zero_na_dropped_transcripts.csv",
+            row.names = FALSE,
+            col.names = FALSE,
+            quote=FALSE)
+write.table(gtex.map.dropped.transcripts %>% gsub("\\..+","",.),
+            file="~/gtex_map_dropped_transcripts.csv",
+            row.names=FALSE,
+            col.names=FALSE,
+            quote=FALSE)
 
 archs4.gene.ids <- archs4.ensembl_gene_map %>%
   dplyr::filter(ensembl_gene_id_version %in% shared.ensembl.gene.ids)
@@ -123,19 +183,16 @@ rownames(archs4.filtered.df) <- archs4.gene.ids$ensembl_gene_id_version
 
 archs4.filtered.df %>% class()
 archs4.filtered.df %>% dim()
-#[1] 12560  4573
 
 tcga.filtered.df <- tcga.df[shared.ensembl.gene.ids,]
 
 tcga.filtered.df %>% class()
 tcga.filtered.df %>% dim()
-#[1] 12560   355
 
 gtex.filtered.df <- gtex.df[shared.ensembl.gene.ids,]
 
 gtex.filtered.df %>% class()
 gtex.filtered.df %>% dim()
-#[1] 12560  1556
 
 combined.df <- archs4.filtered.df %>%
   dplyr::bind_cols(tcga.filtered.df) %>%
@@ -147,7 +204,6 @@ combined.df <- combined.df[rowSums(is.na(combined.df)) == 0,]
 
 combined.df %>% class()
 combined.df %>% dim()
-#[1] 12560  6484
 
 saveRDS(combined.df,file="~/combined_df.Rds")
 
