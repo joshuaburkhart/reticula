f1f9ecb7f7b911ed462a61cf9eca5aa51df86517
burkhart <burkhart.joshua@gmail.com>
2020-05-22 15:43:20 -0700

    adding script for final dataset processing

---
+++ src/r/preprocessing/final_dataset_processing.R
@@ -0,0 +1,47 @@
+library(DESeq2)
+library(magrittr)
+library(SummarizedExperiment)
+
+IN_DIR <- "/home/burkhart/Software/reticula/data/aim1/input/"
+OUT_DIR <- "/home/burkhart/Software/reticula/data/aim1/output/"
+
+#GTEx_DATA_DIR <- "/home/users/burkhajo/WuLab/WuLabLustreDir/reticula/input/recount2/recount2edGTEx/"
+#GTEx_DATA_DIR <- "/Users/burkhajo/Software/reticula/data/aim1/input/recount2/recount2edGTEx/"
+GTEx_DATA_DIR <- "/home/burkhart/Software/reticula/data/aim1/input/recount2/recountedGTEx/"
+GTEx_DATA_FIL <- "rse_gene.Rdata"
+
+ensembl2rxns.df <- read.table(paste(IN_DIR,"Ensembl2ReactomeReactions.txt",sep=""),
+                              sep="\t",
+                              comment.char = "")
+
+load(paste(GTEx_DATA_DIR,GTEx_DATA_FIL,sep=""))
+gtex.cols <- rse_gene %>% colData()
+saveRDS(gtex.cols$smtsd,file=paste(OUT_DIR,"gtex_tissue_detail_vec.Rds",sep=""))
+gtex.tissue.detail.vec <- readRDS(paste(OUT_DIR,"gtex_tissue_detail_vec.Rds",sep=""))
+
+gtex.df <- rse_gene %>% SummarizedExperiment::assay() %>% as.data.frame()
+ensembl_wo_ids <- gsub("\\.[0-9]+","",rownames(gtex.df))
+rownames(gtex.df) <- ensembl_wo_ids
+reactome_ensembl_ids <- intersect(ensembl2rxns.df$V1,ensembl_wo_ids)
+gtex.df <- gtex.df[reactome_ensembl_ids,]
+saveRDS(gtex.df,file=paste(OUT_DIR,"gtex_df.Rds",sep=""))
+gtex.df <- readRDS(paste(OUT_DIR,"gtex_df.Rds",sep=""))
+
+#minimum shrinkage, leaving max() == integer max
+scale.factor <- (.Machine$integer.max - 1) / max(gtex.df)
+gtex.df <- round(gtex.df * scale.factor)
+gtex.df <- gtex.df + 1
+
+dds <- DESeq2::DESeqDataSetFromMatrix(countData = as.matrix(gtex.df),
+                                      colData = data.frame(Sample=colnames(gtex.df),
+                                                           Tissue=gtex.tissue.detail.vec),
+                                      design = ~ Tissue)
+saveRDS(dds,
+        paste(OUT_DIR,"dds.Rds",sep=""))
+
+vst.counts <- DESeq2::vst(dds,
+                          blind = FALSE,
+                          fitType = "local")
+
+saveRDS(vst.counts,
+        paste(OUT_DIR,"vst_counts.Rds",sep=""))
