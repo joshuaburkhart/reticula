e9fc81f4f427f5af01697b2a7fed83166dcb9198
burkhart <burkhart.joshua@gmail.com>
2020-06-04 14:40:09 -0700

    K-means clustering, assignment and ARI calculation executing in approximately 1 second (per rxn).

--- src/r/preprocessing/final_dataset_clustering.R
+++ src/r/preprocessing/final_dataset_clustering.R
@@ -1,12 +1,14 @@
-library(NMF)
+
 library(DESeq2)
-library(fossil)
 library(ggplot2)
 library(viridis)
 library(magrittr)
 library(pheatmap)
+library(pdfCluster)
 library(RColorBrewer)
 library(SummarizedExperiment)
+
+start_time <- Sys.time()
 
 OUT_DIR <- "/home/burkhart/Software/reticula/data/aim1/output/"
 
@@ -19,21 +21,42 @@ rxn2ensembls.nls <- readRDS(paste(OUT_DIR,"rxn2ensembls_nls.Rds",sep=""))
 rxns <- rxn2ensembls.nls %>% names()
 
 #calculate clustering coefficents for each reaction
+rxn_ari.nls <- list()
+count <- 0
 for(rxn_id in rxns){
  ensembl_ids <- rxn2ensembls.nls[[rxn_id]]
  # combined.df columns ordered by clustering
  expr_matrix <- vst.count.mtx[ensembl_ids,]
  km_obj <- kmeans(t(expr_matrix),centers = 54)
- ari <- fossil::adj.rand.index(as.factor(km_obj$cluster),
-                               as.factor(gtex_tissue_detail.vec))
- purity <- NMF::purity(as.factor(km_obj$cluster),
-                       as.factor(gtex_tissue_detail.vec))
+ 
+ km_cluster_calls <- km_obj$cluster
+ km_clust_labels <- km_obj$cluster %>% unique()
+ for(km_clust_label in km_clust_labels){
+         km_clust_label_gtex_annotations <- gtex_tissue_detail.vec[which(km_obj$cluster %in% km_clust_label)]
+         majority_annotation <- km_clust_label_gtex_annotations %>% table() %>% .[1] %>% names()
+         km_cluster_calls[which(km_cluster_calls %in% km_clust_label)] <- majority_annotation
+ }
+ ari <- pdfCluster::adj.rand.index(unname(km_cluster_calls),
+                               gtex_tissue_detail.vec)
+ rxn_ari.nls[[rxn_id]] <- ari
+ count <- count + 1
+ #if(mod(count,100) == 0){
+  print(paste("Last ARI = ",ari,". Clustered ",count," of ",length(rxns)," reactions..."))
+  flush.console()
+ #}
 }
 
-DESeq2::plotPCA(vst.counts,intgroup="Tissue")
+saveRDS(rxn_ari.nls,paste(OUT_DIR,"rxn_ari_nls.Rds",sep=""))
 
-#"ENSG00000010671" "ENSG00000146535"
-p <- ggplot(zt,aes(BTK,GNA12))
-p <- p + geom_point(aes(color = gtex_tissue_detail.vec))
-p <- p + theme(legend.position = "none")
-ggsave("~/test123.png")
+#DESeq2::plotPCA(vst.counts,intgroup="Tissue")
+#purity <- NMF::purity(as.factor(km_obj$cluster),
+#                      as.factor(gtex_tissue_detail.vec))
+#
+##"ENSG00000010671" "ENSG00000146535"
+#p <- ggplot(zt,aes(BTK,GNA12))
+#p <- p + geom_point(aes(color = gtex_tissue_detail.vec))
+#p <- p + theme(legend.position = "none")
+#ggsave("~/test123.png")
+
+end_time <- Sys.time()
+print(paste("Start: ",start_time," End: ",end_time," Difference: ",end_time - start_time))
