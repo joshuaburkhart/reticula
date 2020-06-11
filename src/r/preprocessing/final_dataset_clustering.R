298e969a808974f9e61ed92d47156dc19e2bec9d
burkhart <burkhart.joshua@gmail.com>
2020-06-11 07:36:19 -0700

    adding gini index & transcript counts for DAC figures

--- src/r/preprocessing/final_dataset_clustering.R
+++ src/r/preprocessing/final_dataset_clustering.R
@@ -4,6 +4,7 @@ library(ggplot2)
 library(viridis)
 library(magrittr)
 library(pheatmap)
+library(DescTools)
 library(pdfCluster)
 library(RColorBrewer)
 library(SummarizedExperiment)
@@ -22,6 +23,8 @@ rxns <- rxn2ensembls.nls %>% names()
 
 #calculate clustering coefficents for each reaction
 rxn_ari.nls <- list()
+rxn_gini.nls <- list()
+rxn_ensembl_counts.nls <- list()
 count <- 0
 for(rxn_id in rxns){
  ensembl_ids <- rxn2ensembls.nls[[rxn_id]]
@@ -36,17 +39,31 @@ for(rxn_id in rxns){
          majority_annotation <- km_clust_label_gtex_annotations %>% table() %>% .[1] %>% names()
          km_cluster_calls[which(km_cluster_calls %in% km_clust_label)] <- majority_annotation
  }
- ari <- pdfCluster::adj.rand.index(unname(km_cluster_calls),
+ unnamed_km_cluster_calls <- unname(km_cluster_calls)
+ 
+ # calculate & store adjusted rand index
+ ari <- pdfCluster::adj.rand.index(unnamed_km_cluster_calls,
                                gtex_tissue_detail.vec)
  rxn_ari.nls[[rxn_id]] <- ari
+ 
+ #calculate & store gini index
+ gini <- DescTools::Gini(unnamed_km_cluster_calls)
+ rxn_gini.nls[[rxn_id]] <- gini
+ 
+ #store ensembl transcript count
+ ecount <- length(ensembl_ids)
+ rxn_ensembl_counts.nls[[rxn_id]] <- ecount
+ 
  count <- count + 1
  if(mod(count,100) == 0){
-  print(paste("Last ARI = ",ari,". Clustered ",count," of ",length(rxns)," reactions..."))
+  print(paste("Last ARI:GINI:ECOUNT = ",ari,":",gini,":",ecount,". Clustered ",count," of ",length(rxns)," reactions..."))
   flush.console()
  }
 }
 
 saveRDS(rxn_ari.nls,paste(OUT_DIR,"rxn_ari_nls.Rds",sep=""))
+saveRDS(rxn_gini.nls,paste(OUT_DIR,"rxn_gini_nls.Rds",sep=""))
+saveRDS(rxn_ensembl_counts.nls,paste(OUT_DIR,"rxn_ensembl_counts.nls",sep=""))
 
 end_time <- Sys.time()
 print(paste("Start: ",start_time," End: ",end_time," Difference: ",end_time - start_time))
