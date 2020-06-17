e313b782e9631c5f84f81864868e403f62cf9a15
Joshua Burkhart <burkhart.joshua@gmail.com>
2020-06-17 12:30:02 -0700

    testing pytorch

---
+++ src/lib/drivers/aim2_reactionwise/pytorch_geom_test.py
@@ -0,0 +1,8 @@
+# testing install from https://pytorch-geometric.readthedocs.io/en/latest/notes/installation.html
+# https://pytorch.org/get-started/locally/#macos-version
+# https://www.jetbrains.com/help/pycharm/conda-support-creating-conda-virtual-environment.html
+
+from __future__ import print_function
+import torch
+x = torch.rand(5, 3)
+print(x)
