7b8644fd15d8194918188fd507ef83f40693ec10
Joshua Burkhart <burkhart.joshua@gmail.com>
2020-06-18 11:19:56 -0700

    running custom pytorch geometric example graph

--- src/lib/drivers/aim2_reactionwise/pytorch_geom_test.py
+++ src/lib/drivers/aim2_reactionwise/pytorch_geom_test.py
@@ -3,6 +3,8 @@
 # https://www.jetbrains.com/help/pycharm/conda-support-creating-conda-virtual-environment.html
 
 from __future__ import print_function
+
+import numpy
 from torch_geometric.datasets import Planetoid
 import torch
 import torch.nn.functional as F
@@ -32,68 +34,68 @@ x_list = [torch.tensor([[-1.1, 2.3], [0.5, 21.1], [1.1, 3.9]], dtype=torch.float
           torch.tensor([[2.1, 2.1], [1.5, 7.1], [7.1, 3.2]], dtype=torch.float)
           ]
 
-y_list = [torch.tensor([1]),
-          torch.tensor([3]),
-          torch.tensor([2]),
-          torch.tensor([2]),
-          torch.tensor([3]),
-          torch.tensor([4]),
-          torch.tensor([1]),
-          torch.tensor([2]),
-          torch.tensor([3]),
-          torch.tensor([2]),
-          torch.tensor([3]),
-          torch.tensor([4]),
-          torch.tensor([4])]
+y_list = [1,
+          2,
+          2,
+          2,
+          3,
+          4,
+          1,
+          2,
+          3,
+          2,
+          3,
+          4,
+          4]
 
-data_list = [Data(x=x_list[0],
-                  edge_index=edge_index.t().contiguous(),
-                  y=y_list[0]),
-             Data(x=x_list[1],
-                  edge_index=edge_index.t().contiguous(),
-                  y=y_list[1]),
-             Data(x=x_list[2],
-                  edge_index=edge_index.t().contiguous(),
-                  y=y_list[2]),
-             Data(x=x_list[3],
-                  edge_index=edge_index.t().contiguous(),
-                  y=y_list[3]),
-             Data(x=x_list[4],
-                  edge_index=edge_index.t().contiguous(),
-                  y=y_list[4]),
-             Data(x=x_list[5],
-                  edge_index=edge_index.t().contiguous(),
-                  y=y_list[5]),
-             Data(x=x_list[6],
-                  edge_index=edge_index.t().contiguous(),
-                  y=y_list[6]),
-             Data(x=x_list[7],
-                  edge_index=edge_index.t().contiguous(),
-                  y=y_list[7]),
-             Data(x=x_list[8],
-                  edge_index=edge_index.t().contiguous(),
-                  y=y_list[8]),
-             Data(x=x_list[9],
-                  edge_index=edge_index.t().contiguous(),
-                  y=y_list[9]),
-             Data(x=x_list[10],
-                  edge_index=edge_index.t().contiguous(),
-                  y=y_list[10]),
-             Data(x=x_list[11],
-                  edge_index=edge_index.t().contiguous(),
-                  y=y_list[11]),
-             Data(x=x_list[12],
-                  edge_index=edge_index.t().contiguous(),
-                  y=y_list[12])]
+dataset = [Data(x=x_list[0],
+                edge_index=edge_index.t().contiguous(),
+                y=y_list[0]),
+           Data(x=x_list[1],
+                edge_index=edge_index.t().contiguous(),
+                y=y_list[1]),
+           Data(x=x_list[2],
+                edge_index=edge_index.t().contiguous(),
+                y=y_list[2]),
+           Data(x=x_list[3],
+                edge_index=edge_index.t().contiguous(),
+                y=y_list[3]),
+           Data(x=x_list[4],
+                edge_index=edge_index.t().contiguous(),
+                y=y_list[4]),
+           Data(x=x_list[5],
+                edge_index=edge_index.t().contiguous(),
+                y=y_list[5]),
+           Data(x=x_list[6],
+                edge_index=edge_index.t().contiguous(),
+                y=y_list[6]),
+           Data(x=x_list[7],
+                edge_index=edge_index.t().contiguous(),
+                y=y_list[7]),
+           Data(x=x_list[8],
+                edge_index=edge_index.t().contiguous(),
+                y=y_list[8]),
+           Data(x=x_list[9],
+                edge_index=edge_index.t().contiguous(),
+                y=y_list[9]),
+           Data(x=x_list[10],
+                edge_index=edge_index.t().contiguous(),
+                y=y_list[10]),
+           Data(x=x_list[11],
+                edge_index=edge_index.t().contiguous(),
+                y=y_list[11]),
+           Data(x=x_list[12],
+                edge_index=edge_index.t().contiguous(),
+                y=y_list[12])]
 
 
 class Net(torch.nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        print("num_node_features:",dataset.num_node_features)
-        print("num_classes:",dataset.num_classes)
-        self.conv1 = GCNConv(dataset.num_node_features, 16)  # 2 features per node, 13 examples of the graph
-        self.conv2 = GCNConv(16, dataset.num_classes)  # 13 examples of the graph, 4 classes of graph
+        # print("num_node_features:",dataset.num_node_features)
+        # print("num_classes:",dataset.num_classes)
+        self.conv1 = GCNConv(2, 16)  # dataset.num_node_features, 16)  # 2 features per node, 13 examples of the graph
+        self.conv2 = GCNConv(16, 4)  # dataset.num_classes)  # 13 examples of the graph, 4 classes of graph
 
     def forward(self, data):
         x, edge_index = data.x, data.edge_index
@@ -106,25 +108,28 @@ class Net(torch.nn.Module):
         return F.log_softmax(x, dim=1)
 
 
-dataset = Planetoid(root='/tmp/Cora', name='Cora')
+# dataset = Planetoid(root='/tmp/Cora', name='Cora')
 
 device = torch.device('cpu')
 model = Net().to(device)
 data = dataset[0].to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
 
-print("dataset[0]:",data)
+print("dataset[0]:", data)
 
 model.train()
 for epoch in range(200):
     optimizer.zero_grad()
     out = model(data)
-    loss = F.nll_loss(out[data.train_mask], data.y[data.train_mask])
+    loss = F.nll_loss(out, torch.tensor(numpy.repeat(data.y,3)))
     loss.backward()
     optimizer.step()
 
 model.eval()
 _, pred = model(data).max(dim=1)
-correct = float(pred[data.test_mask].eq(data.y[data.test_mask]).sum().item())
-acc = correct / data.test_mask.sum().item()
+correct = float(pred.eq(data.y).sum().item())
+actual = float(data.x.sum().item())
+acc = correct / actual
+print("Correct:",correct)
+print("Actual:",actual)
 print('Accuracy: {:.4f}'.format(acc))
