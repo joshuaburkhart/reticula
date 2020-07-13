38b199ee7cd1ec9489c4c8670e6f82e412540d16
burkhart <burkhart.joshua@gmail.com>
2020-07-13 12:52:55 -0700

    kmeans parameter sweeping (untested)

--- src/r/preprocessing/final_dataset_clustering.R
+++ src/r/preprocessing/final_dataset_clustering.R
@@ -13,6 +13,19 @@ start_time <- Sys.time()
 
 OUT_DIR <- "/home/burkhart/Software/reticula/data/aim1/output/"
 
+kmeans_maj_vote <- function(expr_matrix,k){ 
+  km_obj <- kmeans(expr_matrix,centers = k)
+
+  km_cluster_calls <- km_obj$cluster
+  km_clust_labels <- km_obj$cluster %>% unique()
+  for(km_clust_label in km_clust_labels){
+    km_clust_label_gtex_annotations <- gtex_tissue_detail.vec[which(km_obj$cluster %in% km_clust_label)]
+    majority_annotation <- km_clust_label_gtex_annotations %>% table() %>% .[1] %>% names()
+    km_cluster_calls[which(km_cluster_calls %in% km_clust_label)] <- majority_annotation
+  }
+  return(unname(km_cluster_calls))
+}
+
 #DESeq2 PCA plot
 vst.counts <- readRDS(paste(OUT_DIR,"vst_counts.Rds",sep=""))
 
@@ -27,23 +40,17 @@ rxn_ari.nls <- list()
 rxn_gini.nls <- list()
 rxn_ensembl_counts.nls <- list()
 count <- 0
+expr_mat <- t(vst.count.mtx[ensembl_ids,])
 for(rxn_id in rxns){
  ensembl_ids <- rxn2ensembls.nls[[rxn_id]]
- # combined.df columns ordered by clustering
- expr_matrix <- vst.count.mtx[ensembl_ids,]
- km_obj <- kmeans(t(expr_matrix),centers = 54)
+ rxn_km_cluster_calls.nls[[rxn_id]] <- c()
  
- km_cluster_calls <- km_obj$cluster
- km_clust_labels <- km_obj$cluster %>% unique()
- for(km_clust_label in km_clust_labels){
-         km_clust_label_gtex_annotations <- gtex_tissue_detail.vec[which(km_obj$cluster %in% km_clust_label)]
-         majority_annotation <- km_clust_label_gtex_annotations %>% table() %>% .[1] %>% names()
-         km_cluster_calls[which(km_cluster_calls %in% km_clust_label)] <- majority_annotation
+ for(k_value in seq(from=31,to=51,by=2)){ # start with number of tissues, odd values up to number of tissue details
+   # load km cluster calls
+   k_calls <- rxn_km_cluster_calls.nls[[rxn_id]]
+   # store km cluster calls
+   rxn_km_cluster_calls.nls[[rxn_id]] <- c(k_calls,kmeans_maj_vote(expr_mat,k_value))
  }
- unnamed_km_cluster_calls <- unname(km_cluster_calls)
- 
- # store km cluster calls
- rxn_km_cluster_calls.nls[[rxn_id]] <- unnamed_km_cluster_calls
  
  # calculate & store adjusted rand index
  ari <- pdfCluster::adj.rand.index(unnamed_km_cluster_calls,
