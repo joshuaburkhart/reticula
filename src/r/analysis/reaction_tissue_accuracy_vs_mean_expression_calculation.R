5b3f4fac7a7ec85e90f5aa2d93ef00f3c76b16e5
burkhart <burkhart.joshua@gmail.com>
2021-04-15 19:02:43 -0700

    utilization works

--- src/r/analysis/reaction_tissue_accuracy_vs_mean_expression_calculation.R
+++ src/r/analysis/reaction_tissue_accuracy_vs_mean_expression_calculation.R
@@ -1,28 +1,49 @@
 set.seed(88888888)
 
 library(magrittr)
+library(ggplot2)
 
 OUT_DIR <- "/home/burkhart/Software/reticula/data/aim1/output/"
 toi_summary.df <- readRDS(file=paste(OUT_DIR,"toi_summary_df.Rds",sep="")) # reaction accuracy X tissue
-vst_count.df <- readRDS(file=paste(OUT_DIR,"vst_count_mtx_train.Rds",sep="")) # transcript expression value X sample
+vst_count.mtx <- as.matrix(readRDS(file=paste(OUT_DIR,"vst_count_mtx_train.Rds",sep=""))) # transcript expression value X sample
 gtex_tissue_detail.vec <- readRDS(file=paste(OUT_DIR,"gtex_tissue_detail_vec_train.Rds",sep="")) # tissue labels for samples
 rxn2ensembl.nls <- readRDS(file=paste(OUT_DIR,"rxn2ensembls_nls.Rds",sep="")) # reaction -> transcript list mapping
 
 tissuewise_dif_mean_rxn_exp.nls <- list()
 
 # calculate difference of mean reaction expression for tissue (mean tissue of interest expression - mean other tissues expression)
-dif_mean_rxn_exp <- function(reaction_id,tissue_label){
-  tissue_sample_idxs <- which(gtex_tissue_detail.vec == tissue_label)
+dif_mean_rxn_exp <- function(reaction_id,tissue_sample_idxs){
   rxn_ensembl_ids <- rxn2ensembl.nls[[reaction_id]]
-  rxn_ensembl_idxs <- which(rownames(vst_count.df) %in% c(rxn_ensembl_ids))
-  mean_rxn_exp_toi <- mean(vst_count.df[rxn_ensembl_idxs,tissue_sample_idxs])
-  mean_rxn_exp_other <- mean(vst_count.df[rxn_ensembl_idxs,-tissue_sample_idxs])
+  rxn_ensembl_idxs <- which(rownames(vst_count.mtx) %in% rxn_ensembl_ids)
+  mean_rxn_exp_toi <- mean(vst_count.mtx[rxn_ensembl_idxs,tissue_sample_idxs])
+  mean_rxn_exp_other <- mean(vst_count.mtx[rxn_ensembl_idxs,-tissue_sample_idxs])
   return(mean_rxn_exp_toi - mean_rxn_exp_other)
 }
 
 for(tissue_label in unique(gtex_tissue_detail.vec)){
   print(paste("Calculating results for ",tissue_label,"...",sep=""))
+  tissue_sample_idxs <- which(gtex_tissue_detail.vec == tissue_label)
   for(reaction_id in names(rxn2ensembl.nls)){
-    tissuewise_dif_mean_rxn_exp.nls[[tissue_label]][[reaction_id]] <- dif_mean_rxn_exp(reaction_id,tissue_label)
+    tissuewise_dif_mean_rxn_exp.nls[[tissue_label]][[reaction_id]] <- dif_mean_rxn_exp(reaction_id,tissue_sample_idxs)
   }
 }
+
+pearson_correlations <- list()
+for(tissue_label in unique(gtex_tissue_detail.vec)){
+name_matches <- tissuewise_dif_mean_rxn_exp.nls[[tissue_label]] %>% names() != rownames(toi_summary.df)
+name_matches %>% as.numeric() %>% sum() == 0
+df <- data.frame(ExprDiff = tissuewise_dif_mean_rxn_exp.nls[[tissue_label]],
+                 RxnAccur = 1 - toi_summary.df[,tissue_label])
+pearson_correlation <- cor(df$ExprDiff,df$RxnAccur)
+pearson_correlations[[tissue_label]] <- pearson_correlation
+print(paste(tissue_label," reaction accuracy vs mean expression difference correlation: ",pearson_correlation,sep=""))
+ggplot2::ggplot(df, aes(x=ExprDiff,y=RxnAccur)) +
+  geom_point() +
+  theme_bw() +
+  theme(legend.position = "none") +
+  ggtitle(paste(tissue_label," (pearson=",pearson_correlation,")",sep=""))
+ggsave(file=paste(OUT_DIR,tissue_label,"_rxn_acc_vs_mean_expr_dif.png",sep=""))
+}
+
+par(mar=c(15,3,1,1))
+pearson_correlations %>% unlist() %>% sort() %>% barplot(las=2,cex.names = 0.75)
