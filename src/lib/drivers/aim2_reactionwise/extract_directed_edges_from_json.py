c784c16d3fca795652a2750b0ce3f08b71fb3b3b
burkhart <burkhart.joshua@gmail.com>
2021-04-22 18:41:40 -0700

    can now extract edges from pathway hierarchy

--- src/lib/drivers/aim2_reactionwise/extract_directed_edges_from_json.py
+++ src/lib/drivers/aim2_reactionwise/extract_directed_edges_from_json.py
@@ -1,15 +1,25 @@
 from networkx.readwrite import json_graph
 import json
+
+#The .json dumped from Reactome is malformed and cannot be consumed in its original state.
+#The below stack trace shows the library errors-out on raw file. Add a parent attribute
+#such as 'Chemistry of Life' to put the text in a compliant/consumable state.
+#Traceback (most recent call last):
+#File "<input>", line 8, in <module>
+#File "/home/burkhart/miniconda3/envs/reticula/lib/python3.7/site-packages/networkx/readwrite/json_graph/tree.py", line 141, in tree_graph
+#root = data[id_]
+#TypeError: list indices must be integers or slices, not str
+
 path = '/home/burkhart/Software/reticula/data/aim2/input/reactome_pathway_hierarchy.json'
 f = open(path)
 data = json.load(f)
 f.close()
+H = json_graph.tree_graph(data, attrs=dict(id='stId', children='children'))
 
-print(data[0])
-print(data[0]['stId'])
-print(data[0]['children'])
-children = data[0]['children']
-for i in children:
-    print(i['stId'])
-    print(i['children'])
+for e in H.edges:
+    parent_id = e[0]
+    child_id = e[1]
+    print("{parent_id} -> {child_id}".format(parent_id=parent_id,child_id=child_id))
+
+
 
