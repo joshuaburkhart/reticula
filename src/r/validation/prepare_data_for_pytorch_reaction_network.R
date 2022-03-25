4858254b5759f3df9e6e0901c5fe0743746e2b4d
jgburk <jgburk@hawaii.edu>
2022-03-24 14:02:52 -1000

    filtering tcga reactions through gtex reactions

--- src/r/validation/prepare_data_for_pytorch_reaction_network.R
+++ src/r/validation/prepare_data_for_pytorch_reaction_network.R
@@ -1,61 +1,23 @@
 library(magrittr)
 
-OUT_DIR <- "/home/burkhart/Software/reticula/data/aim1/output/"
-IN_DIR <- "/home/burkhart/Software/reticula/data/aim2/input/"
+OUT_DIR <- "/home/jgburk/PycharmProjects/reticula/data/tcga/output/"
+IN_DIR <- "/home/jgburk/PycharmProjects/reticula/data/tcga/input/"
+GTEX_DIR <- "/home/jgburk/PycharmProjects/reticula/data/gtex/"
 
 X <- readRDS(paste(OUT_DIR, "rxn_pca_nls.Rds", sep = ""))
-Y <- readRDS(paste(OUT_DIR,"gtex_tissue_detail_vec_train.Rds",sep=""))
-E <- read.table(paste(IN_DIR,"ReactionNetwork_Rel_71_122820.txt",sep=""))
-
-rxn2nodeLabel.nls <- list()
-nodeLabel2rxn.nls <- list()
-for(i in 1:length(X)){
-  rxn2nodeLabel.nls[[names(X)[i]]] <- i
-  nodeLabel2rxn.nls[[i]] <- names(X)[i]
-}
-
-E <- E %>%
-  dplyr::filter(V1 %in% names(rxn2nodeLabel.nls)) %>%
-  dplyr::filter(V3 %in% names(rxn2nodeLabel.nls)) %>%
-  dplyr::select(V1,V3)
-
-write.table(E,
-            file=paste(IN_DIR,"edgeLabels.csv",sep=""),
-            row.names = FALSE,
-            col.names = FALSE)
-
-node1 <- numeric()
-node2 <- numeric()
-for(i in 1:nrow(E)){
-  node1 <- c(node1,rxn2nodeLabel.nls[[as.character(E$V1[i])]])
-  node2 <- c(node2,rxn2nodeLabel.nls[[as.character(E$V3[i])]])
-}
-
-z <- unlist(rxn2nodeLabel.nls)
-y <- unlist(nodeLabel2rxn.nls)
-
-assertthat::are_equal(length(z),length(y))
-
-write.table(z,
-            file=paste(IN_DIR,"rxn2nodeLabel_nls.csv",sep=""),
-            row.names = TRUE,
-            col.names = FALSE)
-write.table(y,
-            file=paste(IN_DIR,"nodeLabel2rxn_nls.csv",sep=""),
-            row.names = TRUE,
-            col.names = FALSE)
-
-E <- data.frame(node1 = node1,
-                node2 = node2)
+Y <- readRDS(paste(OUT_DIR,"tcga_tissue_vec_train.Rds",sep=""))
+gtex_rxn2nodeLabel.df <- read.table(paste(GTEX_DIR,
+                                           "Copy\ of\ rxn2nodeLabel_nls.csv",sep=""),
+                                     sep=" ")
 
 X <- as.data.frame(X)
 
 Y <- as.data.frame(Y)
 
-write.table(E,
-          file=paste(IN_DIR,"edges.txt",sep=""),
-          row.names = FALSE,
-          col.names = FALSE)
+m <- setdiff(make.names(gtex_rxn2nodeLabel.df$V1),colnames(X)) 
+X[m] <- 0
+X <- X[make.names(gtex_rxn2nodeLabel.df$V1)]
+
 write.table(X,
           file=paste(IN_DIR,"node_features.txt",sep=""),
           row.names = FALSE,
