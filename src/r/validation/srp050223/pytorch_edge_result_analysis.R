7f07aa22612ad30d5cb69adba8496a782e96be51
jgburk <jgburk@hawaii.edu>
2022-04-05 10:37:16 -1000

    generating ensembl lists for top weighted reactions for each tissue

--- src/r/validation/srp050223/pytorch_edge_result_analysis.R
+++ src/r/validation/srp050223/pytorch_edge_result_analysis.R
@@ -1,3 +1,4 @@
+library(dplyr)
 library(magrittr)
 
 IN_DIR <- "/home/jgburk/PycharmProjects/reticula/data/SRP050223/output/"
@@ -84,3 +85,108 @@ for(edge_idx in 1:nrow(ig_edge_idx.df)){
 saveRDS(tissuewise_ig_edge_wilcox_res.nls,file=paste(IN_DIR,"tissuewise_ig_edge_wilcox_res_nls.Rds",sep=""))
 
 # note lowest possible wilcoxon p-value for an edge is given by "wilcox.test(x = c(51), y= seq(1:50),alternative = "greater")"
+
+#generate pathway mapping for edges
+REACTION_TO_PTHWY_FN <- "/home/jgburk/PycharmProjects/reticula/data/SRP050223/input/ReactionToPathway_Rel_71_122820.txt" # on box.com
+
+reaction_2_pthwy.df <- read.table(file=REACTION_TO_PTHWY_FN,sep="\t",header = TRUE,
+                                  colClasses = c("character","character"))
+
+pthwy_2_reaction.nls <- list()
+reaction_2_pthwy.nls <- list()
+for(i in 1:nrow(reaction_2_pthwy.df)){
+  rxn <- reaction_2_pthwy.df$ReactionlikeEvent[i]
+  pthwy <- reaction_2_pthwy.df$Pathway[i]
+  if(is.null(pthwy_2_reaction.nls[[pthwy]])){
+    pthwy_2_reaction.nls[[pthwy]] <- rxn
+  }else{
+    pthwy_2_reaction.nls[[pthwy]] <- c(pthwy_2_reaction.nls[[pthwy]],rxn)
+  }
+  if(is.null(reaction_2_pthwy.nls[[rxn]])){
+    reaction_2_pthwy.nls[[rxn]] <- pthwy
+  }else{
+    reaction_2_pthwy.nls[[rxn]] <- c(reaction_2_pthwy.nls[[rxn]],pthwy)
+  }
+}
+
+pthwy_2_edge.nls <- list()
+edge_2_pthwy.nls <- list()
+edge_name_list <- list()
+for(i in 1:nrow(labelled_edge_weights.df)){
+  p_rxn <- labelled_edge_weights.df$Preceeding_Reaction[i]
+  f_rxn <- labelled_edge_weights.df$Following_Reaction[i]
+  edge_name <- paste(p_rxn,"->",f_rxn)
+  edge_name_list[[i]] <- edge_name
+  pthwy_candidates <- reaction_2_pthwy.nls[[p_rxn]]
+  for(pthwy_candidate in pthwy_candidates){
+    if(f_rxn %in% pthwy_2_reaction.nls[[pthwy_candidate]]){
+      if(is.null(pthwy_2_edge.nls[[pthwy_candidate]])){
+        pthwy_2_edge.nls[[pthwy_candidate]] <- edge_name
+      }else{
+        pthwy_2_edge.nls[[pthwy_candidate]] <- c(pthwy_2_edge.nls[[pthwy_candidate]],edge_name)
+      }
+      if(is.null(edge_2_pthwy.nls[[edge_name]])){
+        edge_2_pthwy.nls[[edge_name]] <- pthwy_candidate
+      }else{
+        edge_2_pthwy.nls[[edge_name]] <- c(edge_2_pthwy.nls[[edge_name]],pthwy_candidate)
+      }
+    }
+  }
+  print(paste("Processed ",i," of ",nrow(labelled_edge_weights.df)," edges...",sep=""))
+}
+
+saveRDS(pthwy_2_edge.nls,file=paste(IN_DIR,"pthwy_2_edge_nls.Rds",sep=""))
+saveRDS(edge_2_pthwy.nls,file=paste(IN_DIR,"edge_2_pthwy_nls.Rds",sep=""))
+saveRDS(edge_name_list,file=paste(IN_DIR,"edge_name_list.Rds",sep=""))
+
+pthwy_2_edge.nls <- readRDS(file=paste(IN_DIR,"pthwy_2_edge_nls.Rds",sep=""))
+edge_2_pthwy.nls <- readRDS(file=paste(IN_DIR,"edge_2_pthwy_nls.Rds",sep=""))
+edge_name_list <- readRDS(file=paste(IN_DIR,"edge_name_list.Rds",sep=""))
+
+#calculate edge pathway enrichment
+edge_pathway_enrichment.nls <- list()
+for(pthwy in names(pthwy_2_edge.nls)){
+  for(i in 1:2){
+    significant_edges <- intersect(names(edge_2_pthwy.nls),unlist(edge_name_list[which(tissuewise_ig_edge_wilcox_res.nls[[i]] < ALPHA)]))
+    edges_in_pathway <- edge_2_pthwy.nls[[pthwy]]
+    q = length(intersect(significant_edges,edges_in_pathway))
+    print(paste("significant edges in pathway: ",q,sep=""))
+    m = length(edges_in_pathway)
+    print(paste("edges in pathway: ",m,sep=""))
+    n = length(names(edge_2_pthwy.nls)) - length(edges_in_pathway)
+    print(paste("edges not in pathway: ",n,sep=""))
+    k = length(significant_edges)
+    print(paste("significant edges: ",k,sep=""))
+    p <- stats::phyper(q-1,m,n,k,lower.tail = FALSE)
+    print(paste("p-value: ",p,sep=""))
+    edge_pathway_enrichment.nls[[tissue_name(i - 1)]][[pthwy]] <- p
+  }
+}
+
+saveRDS(edge_pathway_enrichment.nls,file=paste(IN_DIR,"edge_pathway_enrichment_nls.Rds",sep=""))
+
+rxn2ensembls.nls <- readRDS(paste(IN_DIR,"rxn2ensembls_nls.Rds",sep=""))
+TOP_N = 20
+top_weighted_rxns_tall <- labelled_edge_weights.df %>%
+  dplyr::slice_max(labelled_edge_weights.df$`IG_tissue: T cell acute lymphoblastic leukemia`,n=TOP_N) %>%
+  dplyr::select(Preceeding_Reaction)
+
+top_weighted_rxn_tall_ensembls <- sort(unique(unlist(rxn2ensembls.nls[unlist(top_weighted_rxns_tall)])))
+write.table(top_weighted_rxn_tall_ensembls,
+            file=paste(IN_DIR,"top_weighted_rxn_tall_ensembls.txt",sep=""),
+            row.names = FALSE,
+            col.names = FALSE,
+            quote = FALSE,
+            sep="\n")
+
+top_weighted_rxns_thymus <- labelled_edge_weights.df %>%
+  dplyr::slice_max(labelled_edge_weights.df$`IG_tissue: normal thymus`,n=TOP_N) %>%
+  dplyr::select(Preceeding_Reaction)
+
+top_weighted_rxn_thymus_ensembls <- sort(unique(unlist(rxn2ensembls.nls[unlist(top_weighted_rxns_thymus)])))
+write.table(top_weighted_rxn_thymus_ensembls,
+            file=paste(IN_DIR,"top_weighted_rxn_thymus_ensembls.txt",sep=""),
+            row.names = FALSE,
+            col.names = FALSE,
+            quote = FALSE,
+            sep="\n")
