9b72294d9d2a10870153d9853431c58d0639f346
jgburk <jgburk@hawaii.edu>
2022-05-05 11:43:52 -1000

    adding reactionwise & edgewise enrichment calculations

---
+++ src/r/validation/srp035988/calculate_reaction_and_edge_enrichment.R
@@ -0,0 +1,113 @@
+# Review phyper function used in https://github.com/joshuaburkhart/reticula/blob/master/src/r/analysis/hypergeometric_enrichment_analysis.R
+# use phyper to calculate (positive) enrichment with fisher exact p-value (explained here: https://stackoverflow.com/questions/53051977/p-value-from-fisher-test-does-not-match-phyper)
+# #phyper(q - 1, m, n, k, lower.tail = FALSE)
+# q = number of white balls drawn (number of transcripts/reactions shared between selection & pathway) 
+# m = number of white balls in urn (number of transcripts/reactions in pathway)
+# n = number of black balls in urn (number of transcripts/reactions not in pathway)
+# k = number of balls drawn (number of transcripts/reactions in selection)
+
+library(magrittr)
+library(dplyr)
+
+ETA <- 1e-5
+OUT_DIR <- "/home/jgburk/PycharmProjects/reticula/data/SRP035988/output/"
+
+# load rxn2ensembls.nls
+rxn2ensembls.nls <- readRDS(paste(OUT_DIR,"rxn2ensembls_nls.Rds",sep=""))
+
+# load res.df
+res.df <- read.table(file=paste(OUT_DIR,"res_df.csv",sep=""), header = TRUE, stringsAsFactors = FALSE)
+
+# represented reaction enrichment
+
+get_reaction_enrichment_p_value <- function(x,y,z){
+  # x = reaction name
+  # y = significant transcripts
+  # z = all transcripts
+  q = length(intersect(y,rxn2ensembls.nls[[x]]))
+  m = length(rxn2ensembls.nls[[x]])
+  n = length(z) - length(rxn2ensembls.nls[[x]])
+  k = length(y)
+  return(c(paste(x),stats::phyper(q - 1,m,n,k,lower.tail = FALSE)))
+}
+
+# Default Thresholds
+default_thresholds_reaction_enrichment_p_values <- mapply(get_reaction_enrichment_p_value,
+                             names(rxn2ensembls.nls),
+                             res.df %>%
+                               dplyr::filter(Sig_W_Default_Thresholds == TRUE) %>%
+                               dplyr::select(EnsemblID),
+                             res.df %>%
+                               dplyr::select(EnsemblID))
+
+# Strict Thresholds
+strict_thresholds_reaction_enrichment_p_values <- mapply(get_reaction_enrichment_p_value,
+                                                          names(rxn2ensembls.nls),
+                                                          res.df %>%
+                                                            dplyr::filter(Sig_W_Strict_Thresholds == TRUE) %>%
+                                                            dplyr::select(EnsemblID),
+                                                          res.df %>%
+                                                            dplyr::select(EnsemblID))
+
+# Relaxed Thresholds
+relaxed_thresholds_reaction_enrichment_p_values <- mapply(get_reaction_enrichment_p_value,
+                                                          names(rxn2ensembls.nls),
+                                                          res.df %>%
+                                                            dplyr::filter(Sig_W_Relaxed_Thresholds == TRUE) %>%
+                                                            dplyr::select(EnsemblID),
+                                                          res.df %>%
+                                                            dplyr::select(EnsemblID))
+
+# check "R-HSA-8956184" = 0.01576931
+spot_test <- relaxed_thresholds_reaction_enrichment_p_values %>% t() %>% as.data.frame() %>% .[.$V1=="R-HSA-8956184",]
+stopifnot(assertthat::are_equal(abs(as.numeric(spot_test$V2) - 0.01576931) < ETA,TRUE))
+
+# edge enrichment
+
+# load labelled_edge_weights.df
+labelled_edge_weights.df <- read.table(paste(OUT_DIR,"labelled_edge_weights.csv",sep=""),
+                                       stringsAsFactors = FALSE,sep = ",",header = TRUE)
+
+get_edge_enrichment_p_value <- function(w,x,y,z){
+  # w = preceding reaction name
+  # x = following reaction name
+  # y = significant transcripts
+  # z = all transcripts
+  edge_rxns = unique(c(rxn2ensembls.nls[[w]],
+                       rxn2ensembls.nls[[x]]))
+  q = length(intersect(y,edge_rxns))
+  m = length(edge_rxns)
+  n = length(z) - length(edge_rxns)
+  k = length(y)
+  return(c(paste(w,x,sep=" -> "),stats::phyper(q - 1,m,n,k,lower.tail = FALSE)))
+}
+
+# Default Thresholds
+default_thresholds_edge_enrichment_p_values <- mapply(get_edge_enrichment_p_value,
+                             labelled_edge_weights.df$Preceeding_Reaction,
+                             labelled_edge_weights.df$Following_Reaction,
+                             res.df %>%
+                               dplyr::filter(Sig_W_Default_Thresholds == TRUE) %>%
+                               dplyr::select(EnsemblID),
+                             res.df %>%
+                               dplyr::select(EnsemblID))
+
+# Strict Thresholds
+strict_thresholds_edge_enrichment_p_values <- mapply(get_edge_enrichment_p_value,
+                                                      labelled_edge_weights.df$Preceeding_Reaction,
+                                                      labelled_edge_weights.df$Following_Reaction,
+                                                      res.df %>%
+                                                        dplyr::filter(Sig_W_Strict_Thresholds == TRUE) %>%
+                                                        dplyr::select(EnsemblID),
+                                                      res.df %>%
+                                                        dplyr::select(EnsemblID))
+
+# Relaxed Thresholds
+relaxed_thresholds_edge_enrichment_p_values <- mapply(get_edge_enrichment_p_value,
+                                                      labelled_edge_weights.df$Preceeding_Reaction,
+                                                      labelled_edge_weights.df$Following_Reaction,
+                                                      res.df %>%
+                                                        dplyr::filter(Sig_W_Relaxed_Thresholds == TRUE) %>%
+                                                        dplyr::select(EnsemblID),
+                                                      res.df %>%
+                                                        dplyr::select(EnsemblID))
---
+++ src/r/validation/srp035988/plot_samples_in_3D_reaction_pca_point_cloud.R
@@ -0,0 +1,5 @@
+# Review plot_ly function used in https://github.com/joshuaburkhart/reticula/blob/master/src/r/validation/srp035988/pca_and_knn_calculation.R 
+
+# extract unique reactions from edges of hub reaction
+
+# 3d plot of those reactions pc1 values colored by phenotype
