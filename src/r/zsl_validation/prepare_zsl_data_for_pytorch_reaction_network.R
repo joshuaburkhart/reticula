dc96ee9c3c5a7081bf2d5f9960bdea9f1833be53
jgburk <jgburk@hawaii.edu>
2022-05-10 10:10:39 -1000

    updating paths for zsl

--- src/r/zsl_validation/prepare_zsl_data_for_pytorch_reaction_network.R
+++ src/r/zsl_validation/prepare_zsl_data_for_pytorch_reaction_network.R
@@ -4,8 +4,8 @@ OUT_DIR <- "/home/jgburk/PycharmProjects/reticula/data/tcga/output/"
 IN_DIR <- "/home/jgburk/PycharmProjects/reticula/data/tcga/input/"
 GTEX_DIR <- "/home/jgburk/PycharmProjects/reticula/data/gtex/"
 
-X <- readRDS(paste(OUT_DIR, "rxn_pca_nls.Rds", sep = ""))
-Y <- readRDS(paste(OUT_DIR,"tcga_tissue_vec_train.Rds",sep=""))
+X <- readRDS(paste(OUT_DIR, "zsl_rxn_pca_nls.Rds", sep = ""))
+Y <- readRDS(paste(OUT_DIR,"zsl_tcga_tissue_vec_train.Rds",sep=""))
 gtex_rxn2nodeLabel.df <- read.table(paste(GTEX_DIR,
                                            "Copy\ of\ rxn2nodeLabel_nls.csv",sep=""),
                                      sep=" ")
@@ -21,11 +21,11 @@ X[m] <- 0
 X <- X[make.names(gtex_rxn2nodeLabel.df$V1)]
 
 write.table(X,
-          file=paste(IN_DIR,"node_features.txt",sep=""),
+          file=paste(IN_DIR,"zsl_node_features.txt",sep=""),
           row.names = FALSE,
           col.names = FALSE)
 write.table(Y,
-          file=paste(IN_DIR,"graph_targets.txt",sep=""),
+          file=paste(IN_DIR,"zsl_graph_targets.txt",sep=""),
           row.names = FALSE,
           col.names = FALSE)
 
@@ -33,7 +33,7 @@ call_check.df <- data.frame(Sample_ID = rownames(X),
                             Tissue = Y$`Y[tissue_alphabetical_order]`,
                             Dataset = rep(c("Tuning Set","Test Set"),length(X[,1])/2))
 write.table(call_check.df,
-            file=paste(OUT_DIR,"call_check.csv",sep=""),
+            file=paste(OUT_DIR,"zsl_call_check.csv",sep=""),
             row.names = FALSE,
             col.names = TRUE,
             sep = ",")
