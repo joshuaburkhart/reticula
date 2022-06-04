45eaed3743dbaa1dd34f4f361bcf508011ed742c
jgburk <jgburk@hawaii.edu>
2022-06-03 15:45:46 -1000

    adding shell scripts for zsl

---
+++ src/sh/prepare_mana_env.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Go Interactive
+#sh ~/go_interactive_gpus.sh
+module purge
+module load lang/Python/3.9.5-GCCcore-10.3.0
+module load system/CUDA/11.0.2
+cd ~/zsl_validation/
+python ReactomeGraphClassificationZSLValidationGTEX_Mana.py
+
+#Editing
+#module purge
+#module load tools/Vim
+
+#Monitor jobs on hpc
+#squeue -o "%all" | head -n1
+
+#Monitor user “anirvan”
+#squeue -o "%all" | grep gpu | grep anirvan | head -n1
---
+++ src/sh/transfer_zsl_logic_to_hpc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir /tmp/zsl_validation
+cd /tmp/zsl_validation/
+cp ~/PycharmProjects/reticula/data/gtex/input/edges.txt ./
+cp ~/PycharmProjects/reticula/data/gtex/input/zsl_gtex_* ./
+cp ~/PycharmProjects/reticula/data/tcga/input/zsl_tcga_* ./
+cp ~/PycharmProjects/reticula/src/sh/go_interactive_gpus.sh ./
+cp ~/PycharmProjects/reticula/src/python/drivers/zsl_validation/gtex/ReactomeGraphClassificationZSLValidationGTEX_Mana.py ./
+cp ~/PycharmProjects/reticula/src/python/drivers/zsl_validation/tcga/ReactomeGraphClassificationZSLValidationTCGA_Mana.py ./
+cd ../
+zip -rj zsl_validation.zip zsl_validation
+. ~/.profile
+huscp zsl_validation.zip ~/
