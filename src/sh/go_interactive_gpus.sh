955be451cb2b809ce4a7e561200b42f735ae29f5
jgburk <jgburk@hawaii.edu>
2022-06-06 10:37:39 -1000

    updating gpu interactive session script for 6 hours of execution

--- src/sh/go_interactive_gpus.sh
+++ src/sh/go_interactive_gpus.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # go interactive before doing anything on the hpc
 # srun -I30 -p kill-shared --gpus-per-node=8 -N 1 -c 8 --mem=32G -t 0-01:00:00 --pty /bin/bash
-srun -I30 -p gpu --gres=gpu:8 -N 1 -c 8 --mem=32G -t 0-01:00:00 --pty /bin/bash
+srun -I30 -p gpu --gres=gpu:8 -N 1 -c 8 --mem=32G -t 0-06:00:00 --pty /bin/bash
 
 
