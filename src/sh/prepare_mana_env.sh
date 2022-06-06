65057767851578bebaf9767291c05b9ad3fc194b
jgburk <jgburk@hawaii.edu>
2022-06-06 08:18:28 -1000

    ignoring useless command

--- src/sh/prepare_mana_env.sh
+++ src/sh/prepare_mana_env.sh
@@ -2,7 +2,9 @@
 
 #Go Interactive
 #sh ~/go_interactive_gpus.sh
-unzip ./zsl_validation.zip -d zsl_validation/
+
+#unzip ./zsl_validation.zip -d zsl_validation/
+
 module purge
 module load lang/Python/3.9.5-GCCcore-10.3.0
 module load system/CUDA/11.0.2
